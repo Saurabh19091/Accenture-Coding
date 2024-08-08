5ec8840b1d5143e84639181d8b1b2e1ba7b6421e
saurabh19091 <saudanger8291@gmail.com>
2024-08-08 10:49:11 +0530

    today commit

---
+++ bubblesort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class bubblesort {
+    public static void main(String[] args) {
+
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+            printa(arr);
+        }
+    }
+
+    public static void printa(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ selectiosort.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class SelectionSort {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int mini = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[mini];
+            arr[mini] = temp;
+
+            // Print the array after each iteration
+            printa(arr);
+        }
+
+        sc.close();
+    }
+
+    // Method to print the array
+    public static void printa(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
